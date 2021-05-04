a592387a59ab75c2dab7b0b9b8dae7037b9a5784
Natacha <natacha@natacha.guijarro64@orange.fr>
2021-05-04 17:12:14 +0200

    Router

---
+++ src/components/NavBar.vue
@@ -0,0 +1,27 @@
+<template>
+  <div id="#app">
+    <h2>NOM SITE</h2>
+    <div class="topnav">
+      <router-link  class="active" to="/">Home</router-link>
+      <router-link to="/user">Compte</router-link>
+      <router-link to="/deco">Déconnexion</router-link>
+    </div>
+  <router-view/> 
+  </div>
+</template>
+
+<script>
+
+export default {
+  name: 'NavBar',
+  props: {
+    page: Number
+  }
+}
+
+
+</script>
+
+<style>
+  @import './../assets/css/style.css';
+</style>
--- src/main.js
+++ src/main.js
@@ -1,4 +1,25 @@
 import { createApp } from 'vue'
-import App from './App.vue'
+import { createRouter, createWebHistory } from 'vue-router'
+import Home from './views/Home.vue'
 
-createApp(App).mount('#app')
+const routes = [
+  {
+    path: '/',
+    name: 'home',
+    component: Home
+  },
+  {
+    path: '/user',
+    name: 'user',
+    component: () => import('./views/User.vue')
+  }
+]
+
+const router = createRouter({
+  history: createWebHistory(process.env.BASE_URL),
+  routes
+})
+
+export default router;
+
+createApp(Home).use(router).mount('#app')
