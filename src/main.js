3ac1f980894643849b85589249f706c699ca22cf
Natacha <natacha@natacha.guijarro64@orange.fr>
2021-05-05 18:30:55 +0200

    Onglet actif

--- src/main.js
+++ src/main.js
@@ -23,7 +23,7 @@ const router = createRouter({
   history: createWebHistory(process.env.BASE_URL),
   routes,
   linkActiveClass: "active",
-  linkExactActiveClass: "exact-active"
+  
 })
 
 export default router
